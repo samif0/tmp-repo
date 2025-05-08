9f5a49667609dd94d38b314330a35c56a6e0df06
Sami Fawcett <samifawcett.nyc@gmail.com>
2025-05-08 04:57:55 +0000

    fiddling with proejct structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import { Geist } from 'next/font/google'
+import BlackFlowHomeHeader from '@/components/BlackFlowHomeHeader'
 
-const geist = Geist({
-  subsets: ['latin'],
-  weight: ['900'],
-})
-
-export default function Home() {
+export default function App() {
   return (
-    <div className="flex flex-col min-h-screen p-12 text-white bg-gradient-to-b from-neutral-950 to-neutral-900 ">
-      <h1 className={`${geist.className} items-center justify-center text-center text-5xl antialiased 
-        md:subpixel-antialiased text-shadow-lg text-shadow-emerald-700`}>
-        BlackFlow
-      </h1>
+    <div>
+      <BlackFlowHomeHeader />
     </div>
   );
 }
